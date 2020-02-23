43adb7aeba1a73936505c1f110155d42619f07aa
balintkaszas <balint.kaszas.7cb@gmail.com>
2020-02-23 14:18:44 +0100

    attempt2

--- finiteDIff_cons.m
+++ finiteDIff_cons.m
@@ -1,23 +1,20 @@
 domain = [-1.5, 1.5; -1.5, 1.5];
-resolution = [1000,1000];
-difference = 3./1000;
+resolution = [200,200];
+difference = 3./200;
 initialPosition = initialize_ic_grid(resolution, domain, 2);
 
 %domain = [-1.5,-0.9;-1, -0.4];
 domain = [-1.5, 1.5;-1.5, 1.5];
-coords = reshape(initialPosition, [1000,1000,2]);
+coords = reshape(initialPosition, [200,200,2]);
 xi = coords(:,:,1);
 yi = coords(:,:,2);
-asd = load('Duffing_0_4pi_1kby1k.mat');
+asd = load('Duff_cons_200by200_0_4pi.mat');
 ftles = asd.ftle;
 FF = repmat(ftles,1,1,3);
 FF=smooth3(FF,'gaussian',3);
 smoothFT = FF(:,:,1);
 
 
-
-%surf(xi,yi,FF(:,:,1));shading interp; axis equal;axis tight;colorbar;
-%view([0 0 1]); axis equal; axis tight; shading interp;camlight
 
 [fx,fy] = gradient(smoothFT, difference);
 %imagesc(domain(:,1), domain(:,2), fx);
@@ -30,6 +27,9 @@ eigenvectorx = fyy - eigenvalsSmall;
 eigenvectory = fyx;
 magnitudes = sqrt(fx.^2 + fy.^2);
 
+
+surf(xi,yi,magnitudes);shading interp; axis equal;axis tight;colorbar;
+view([0 0 1]); axis equal; axis tight; shading interp;camlight
 
 
 %imagesc(domain(1,:), domain(2,:), eigenvalsSmall);
@@ -65,20 +65,20 @@ eigenvectorySmallInterp = griddedInterpolant(xi,yi,eigenvectory, 'spline');
 hold on;
 %imagesc(domain(1,:), domain(2,:), ftles);
 %Opt    = odeset('Events', @event);
-eventfonction = @(t, y) eventss(t, y, @(x1, x2) eigenvalueSmallInterp(x1, x2), @(x1, x2) eigenvectorxSmallInterp(x1, x2), @(x1, x2) eigenvectorySmallInterp(x1, x2), @(x1, x2) fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2));
-optionsode=odeset('Events',eventfonction);
-
-%[t,sol] = ode45(@(t,e) odefun(t,e, @(x1, x2)fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2) ), [0, 0.1*pi], [maskedx(10), maskedy(10)], optionsode);
+% eventfonction = @(t, y) eventss(t, y, @(x1, x2) eigenvalueSmallInterp(x1, x2), @(x1, x2) eigenvectorxSmallInterp(x1, x2), @(x1, x2) eigenvectorySmallInterp(x1, x2), @(x1, x2) fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2));
+% optionsode=odeset('Events',eventfonction);
+% 
+% %[t,sol] = ode45(@(t,e) odefun(t,e, @(x1, x2)fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2) ), [0, 0.1*pi], [maskedx(10), maskedy(10)], optionsode);
 %plot(sol(:,1), sol(:,2), '.', 'color', 'black','LineWidth', 4)
-
-for i = 5:17
-    [~,sol] = ode45(@(t,e) odefun(t,e, @(x1, x2)fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2) ), [0, pi], [maskedx(i), maskedy(i)], optionsode);
-    %plot(maskedx, maskedy, 'o');
-    plot(sol(:,1), sol(:,2), '.', 'color', 'black','LineWidth', 4)
-    %if(norm(sol(end)) > 1e10)
-        %disp(norm(sol(end)))
-    %end
-end
+% 
+% for i = 5:17
+%     [~,sol] = ode45(@(t,e) odefun(t,e, @(x1, x2)fxInterp(x1, x2), @(x1, x2) fyInterp(x1, x2) ), [0, pi], [maskedx(i), maskedy(i)], optionsode);
+%     %plot(maskedx, maskedy, 'o');
+%     plot(sol(:,1), sol(:,2), '.', 'color', 'black','LineWidth', 4)
+%     %if(norm(sol(end)) > 1e10)
+%         %disp(norm(sol(end)))
+%     %end
+% end
 %eigenvalueSmallInterp(3, 4);
 function [value, isterminal, direction] = eventss(T, X, eigenvalueSmallInterp, eigenvectorxSmallInterp, eigenvectorySmallInterp, fxInterp, fyInterp)
 x = X(1);
